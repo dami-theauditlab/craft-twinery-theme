e8ea51d9757dcbe87f49bf0eb7a9235553e67e96
dami-theauditlab <dami@theauditlab.com>
2022-12-21 11:50:19 +0000

    updating product single page

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -75,36 +75,27 @@ if ( post_password_required() ) {
 			?>
 		</div>
 	</div>
-
-</div>
 	</div>
-    <section id="account-block">
-    <div class="container">
-        <div class="cart-div">
-            <span class="title">
-                <img src="<?php bloginfo('template_url'); ?>/images/basket.png" alt="Basket Icon">
-                <h2>Your Cart</h2>
-            </span>
-            <p><?php echo count(WC()->cart->get_cart()); ?> Products in cart</p>
-            <p>Total <strong>£<?php echo WC()->cart->get_cart_contents_total(); ?></strong></p>
-            <a href="/basket"><div class="button">CHECKOUT</div></a>
-        </div>
-        <div class="account-div">
-            <span class="title">
-                <img src="<?php bloginfo('template_url'); ?>/images/lock.png" alt="Lock Icon">
-                <h2>Your Account</h2>
-            </span>
-            <?php if(is_user_logged_in()) { ?>
-                <p>Logged in</p>
-                <a href="/my-account/"><div class="button">MY ACCOUNT</div></a>
-            <?php } ?>
-            <?php if(!is_user_logged_in()) { ?>
-                <p>Not logged in</p>
-                <a href="/my-account/"><div class="button">LOGIN</div></a>
-            <?php } ?>
-        </div>
-    </div>
+</div>
+    
+<section class="product-share">
+	<div class="container">
+		<div class="text">
+			<h2>Have a question?<br> Want to share with a friend? Wish to write a review?</h2>
+			<p>Click on the relative box below to contact us, share the product or write a review on how the product was...</p>
+			<div class="buttons">
+				<div>CONTACT US</div>
+				<div>EMAIL FRIEND</div>
+				<div>REVIEW</div>
+			</div>
+		</div>
+		<div class="image"><img src="<?php bloginfo('template_url'); ?>/images/share-image.jpg" alt="Image with different colour strings"></div>
+	</div>
+
 </section>
+
+
+
 </section>
 <?php do_action( 'woocommerce_after_single_product' ); ?>
 
